032313e065794464b10dd3e50e26a84bbf044ec7
martin brook <martin.brook100@googlemail.com>
2013-11-17 12:09:44 -0800

    Test OBS build result polling 4

--- example-platform-app.c
+++ example-platform-app.c
@@ -2,6 +2,6 @@
 
 int main(int argc, char **argv)
 {
-    printf("Hello, JLR Tizen! - Test OBS build result polling 3\n");
+    printf("Hello, JLR Tizen! - Test OBS build result polling 4\n");
     return 0;
 }
